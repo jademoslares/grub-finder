2a8d418d927d1990e6bfb800d049bed212234f67
RACLesty <ztos@msn.com>
2024-04-12 08:41:27 -0400

    Swipe Motion

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -9,14 +9,15 @@ export default function NavBar({ user, setUser }) {
 
   return (
     <nav>
+      <span>Welcome, {user.username}</span>
+      &nbsp; | &nbsp;
+      <Link to="/restaurants">Restaurants</Link>
+      &nbsp; | &nbsp;
       <Link to="/orders">Order History</Link>
       &nbsp; | &nbsp;
       <Link to="/orders/new">New Order</Link>
       &nbsp; | &nbsp;
-      <Link to="/restaurants">Restaurants</Link>
-      &nbsp; | &nbsp;
-      <span>Welcome, {user.username}</span>
-      &nbsp;&nbsp;<Link to="" onClick={handleLogOut}>Log Out</Link>
+      <Link to="" onClick={handleLogOut}>Log Out</Link>
     </nav>
   );
 }
--- src/pages/RestaurantPage/RestaurantPage.jsx
+++ src/pages/RestaurantPage/RestaurantPage.jsx
@@ -1,5 +1,49 @@
+import React, {useState} from 'react';
+import './RestaurantPage.css';
+import CardRestaurant from 'react-tinder-card'
+
 export default function RestaurantPage() {
+
+    const [restaurant, setRestaurant] = useState([
+        {
+            name: 'Restaurant 1',
+            url: 'https://source.unsplash.com/random/1000x1000?restaurant',
+        },
+        {
+            name: 'Restaurant 2',
+            url: 'https://source.unsplash.com/random/1000x1000?restaurant',
+        },
+    ]);
+
+
+
     return (
-      <h1>Restaurant</h1>
+        <div className='CardTinder'>
+
+            <div className='cardTinder_container'>
+                {restaurant.map(restaurant => (
+
+                    <CardRestaurant
+                    className='swipe'
+                    key={restaurant.name}
+                    preventSwipe={['up', 'down']}
+                    >
+
+                        <div 
+                        className='card'
+                        style={{backgroundImage: `url(${restaurant.url}`}}
+                        >
+
+                            <h2>{restaurant.name}</h2>
+
+                        </div>
+
+                    </CardRestaurant>
+
+
+                ))}
+            </div>
+
+        </div>
     );
   }
