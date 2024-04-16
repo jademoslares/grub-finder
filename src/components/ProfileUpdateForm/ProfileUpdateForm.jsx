3c376ab684d3ce97fab8b45e018ca31f46d685a9
jademoslares <jade.moslares@gmail.com>
2024-04-16 15:41:27 -0400

    Secret Hid

--- src/components/ProfileUpdateForm/ProfileUpdateForm.jsx
+++ src/components/ProfileUpdateForm/ProfileUpdateForm.jsx
@@ -84,8 +84,8 @@ export default function ProfileUpdateForm({ user, setUpdateForm }) {
 
   const uploadFileToS3 = async (file) => {
     const s3 = new AWS.S3({
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
       region: 'us-east-2',
     });
 
