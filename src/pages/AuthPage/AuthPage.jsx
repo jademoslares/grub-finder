c584e0fadc7ddabf303ff71651ae3c7a79ba59d1
Jessica <jessicamendez328@gmail.com>
2024-04-16 16:04:49 -0400

    nav tweaks

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,18 +1,25 @@
 import { useState } from 'react';
 import SignUpForm from '../../components/SignUpForm/SignUpForm';
 import LoginForm from '../../components/LoginForm/LoginForm';
+import '../App/App.css';
 
 export default function AuthPage({ setUser }) {
   const [showSignUp, setShowSignUp] = useState(false);
   return (
-    <main>
-      <h1>AuthPage</h1>
+    <main className='lander'>
+      <div className='grub'>
+     🔍🐛Grub Finder
+      🍽️ We know where to look 
+      </div>
+
+      <div className='lander-form'>
       <button onClick={() => setShowSignUp(!showSignUp)}>{showSignUp ? 'Log In' : 'Sign Up'}</button>
       { showSignUp ?
           <SignUpForm setUser={setUser} />
           :
           <LoginForm setUser={setUser} />
       }
+      </div>
     </main>
   );
 }
