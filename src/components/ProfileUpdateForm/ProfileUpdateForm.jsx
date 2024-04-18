6c49ea632e0699c74744d54e07ea176ee369c9b9
jademoslares <jade.moslares@gmail.com>
2024-04-18 16:15:26 -0400

    change

--- src/components/ProfileUpdateForm/ProfileUpdateForm.jsx
+++ src/components/ProfileUpdateForm/ProfileUpdateForm.jsx
@@ -83,8 +83,8 @@ export default function ProfileUpdateForm({ user, setUpdateForm }) {
 
   const uploadFileToS3 = async (file) => {
     const s3 = new AWS.S3({
-      accessKeyId: 'AKIA5FTZCU47WYBWDPKO',
-      secretAccessKey: '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',
+      accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
       region: 'us-east-2',
     });
 
@@ -94,7 +94,7 @@ export default function ProfileUpdateForm({ user, setUpdateForm }) {
     const newFileName = `${uniqueId}${fileExtension}`;
 
     const params = {
-      Bucket: 'grubfinder-storage',
+      Bucket: process.env.REACT_APP_S3_BUCKET,
       Key: newFileName, // Key under which to store the file
       Body: file,
     };
